115ab4c5bb871a8c7baf319f4a042ce6d9ccf018
Diego Vaschetto <diego_vaschetto@outlook.it>
2022-10-19 23:53:55 +0200

    finish template

--- src/app/pages/drink/drink.component.ts
+++ src/app/pages/drink/drink.component.ts
@@ -1,41 +1,67 @@
-import { Component, OnInit } from "@angular/core"
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
+
+interface DrinkProp extends Record<string, any> {
+    idDrink: string;
+    strDrink: string;
+    strInstructionsIT: string;
+    strDrinkThumb: string;
+}
 
 interface DrinkApi {
-    drinks:[]
+    drinks: DrinkProp[];
+}
+
+interface IngredientAndMeasure {
+    name: string
+    measure : string
 }
 
 @Component({
-    selector:"app-drink",
-    templateUrl:"./drink.component.html",
-    styleUrls:["./drink.component.scss"]
+    selector: 'app-drink',
+    templateUrl: './drink.component.html',
+    styleUrls: ['./drink.component.scss'],
 })
 class DrinkComponent implements OnInit {
+    constructor(private http: HttpClient, private route: ActivatedRoute) {}
 
-    constructor(private http:HttpClient, private route: ActivatedRoute ){
-    }
+    idDrink: string = '';
+    ingredientsList: IngredientAndMeasure[] = [];
+    drink : DrinkProp | any
 
-    idDrink:any=""
-    drink:DrinkApi = {drinks:[]}
-    
     ngOnInit(): void {
-        const urlRoot= "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="
-        this.route.paramMap.subscribe(params => {
-            this.idDrink = params.get('idDrink')
-          })
+        const urlRoot =
+            'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+        this.route.paramMap.subscribe((params) => {
+            this.idDrink = params.get('idDrink') as string;
+        });
 
         this.http
-            .get(urlRoot+this.idDrink)
-            .subscribe((response:Partial<DrinkApi>) => {
-                console.log(response)
-            })
-        
+            .get(urlRoot + this.idDrink)
+            .subscribe((response: Partial<DrinkApi>) => {
+                this.drink = response.drinks![0];
+                this.ingredients();
+            });
     }
 
-    
-    
+    ingredients = () => {
+        for (const prop in this.drink) {
+            if (prop.includes('strIngredient') && this.drink[prop]) {
+                let index = prop.slice(prop.length - 1);
+                let ingredientAndMeasure = {
+                    name: this.drink[prop],
+                    measure: this.drink['strMeasure' + index]
+                };
+                
+                this.ingredientsList.push(ingredientAndMeasure)
+            }
+        }
+    }
 
+    printIngredients = (ingredient:IngredientAndMeasure) => {
+        return(ingredient.name + " " + ingredient.measure)
+    }
 }
 
-export default DrinkComponent
+export default DrinkComponent;
